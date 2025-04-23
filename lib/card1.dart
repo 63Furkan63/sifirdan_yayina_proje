11a8f33a9aabbb0e7c46dc511cedcb94fbaf54fd
frknklc.632008 <frknklc.632008@gmail.com>
2025-04-23 20:29:38 +0000

    card yapısı bitt

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/theme_islemleri.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({super.key});
   final String kategori = 'Editörün seçimi';
   final String baslik = 'Hamur sanatı';
-  final String aciklama = 'Mükemmel ekmeği yapmayı öğrenin';
+  final String aciklama = 'Mükemmel yemek yapmayı öğrenin!';
   final String sef = 'M.Furkan KILIÇ';
   @override
   Widget build(BuildContext context) {
@@ -18,6 +19,31 @@ class Card1 extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Stack(
+          children: [
+            Text(kategori, style: TemaIslemleri.acikYaziTema.bodyMedium),
+            Positioned(
+              top: 22,
+              child: Text(
+                baslik,
+                style: TemaIslemleri.acikYaziTema.headlineSmall,
+              ),
+            ),
+            Positioned(
+              bottom: 28,
+              right: 0,
+              child: Text(
+                aciklama,
+                style: TemaIslemleri.acikYaziTema.headlineSmall,
+              ),
+            ),
+            Positioned(
+              bottom: 5,
+              right: 0,
+              child: Text(sef, style: TemaIslemleri.acikYaziTema.headlineSmall),
+            ),
+          ],
         ),
       ),
     );
